3ed8c1f945fa2521ce52e2054c8b49bbe9590d1d
Steve Hsu <triangular.pyramid@gmail.com>
2017-06-04 16:59:24 -0400

    added lynxrc

--- symlink_setdown.sh
+++ symlink_setdown.sh
@@ -4,6 +4,8 @@ function remove_link {
 	if test -L $1
 	then
 		rm --verbose $1
+	else
+		echo $1 'is not a symbolic link'
 	fi
 }
 
@@ -11,8 +13,9 @@ remove_link ${HOME}/.zshrc
 remove_link ${HOME}/.zsh_aliases
 remove_link ${HOME}/.zsh_prompt
 remove_link ${HOME}/.vim/vimrc
+remove_link ${HOME}/.lynxrc
 remove_link ${HOME}/.ssh/config
 remove_link ${HOME}/.tmux.conf
-remove_link ${HOME}/.gitconfig
+remove_link ${HOME}/.config/git/config
 remove_link ${HOME}/.config/sway/config
 remove_link ${HOME}/.sway_env
