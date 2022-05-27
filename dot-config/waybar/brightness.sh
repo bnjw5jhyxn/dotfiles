3f7b9c72271c3d87bf461db33bf356958499c804
Steve Hsu <triangular.pyramid@gmail.com>
2022-05-27 11:58:13 -0400

    new laptop May 2022

--- dot-config/waybar/brightness.sh
+++ dot-config/waybar/brightness.sh
@@ -3,8 +3,8 @@
 n=$(cat /sys/class/backlight/intel_backlight/brightness)
 if test $1 = up
 then
-	echo $((n + 75)) > /sys/class/backlight/intel_backlight/brightness
+	echo $((n + 960)) > /sys/class/backlight/intel_backlight/brightness
 elif test $1 = down
 then
-	echo $((n - 75)) > /sys/class/backlight/intel_backlight/brightness
+	echo $((n - 960)) > /sys/class/backlight/intel_backlight/brightness
 fi
